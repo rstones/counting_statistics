2394306f553272da3640291b8d558fd0b4f56d4b
Rising Up! <richardstones10@gmail.com>
2018-11-26 10:55:02 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="counting_statistics",
+    version="0.1",
+    author_email="richardstones10@gmail.com",
+    description="A Python library for calculating the electron counting statistics of Markovian and non-Markovian systems.",
+    packages=['counting_statistics']
+)
