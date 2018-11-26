6832860ba8027d19dc4771e4a50d97a6ccbe2783
Rising Up! <richardstones10@gmail.com>
2018-11-26 11:07:27 +0000

    setup.py fix

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.1",
     author_email="richardstones10@gmail.com",
     description="A Python library for calculating the electron counting statistics of Markovian and non-Markovian systems.",
-    packages=['counting_statistics', 'counting_statistcs.sparse']
+    packages=['counting_statistics', 'counting_statistics.sparse']
 )
